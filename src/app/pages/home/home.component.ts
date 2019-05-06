087d1fc53167c911c52653d994178ec67a50f8d1
Wahyu Budi Saputra <wahyubucil@gmail.com>
2019-05-07 00:38:27 +0800

    Handle home animation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+  loaded: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    setTimeout(() => (this.loaded = true), 300);
   }
-
 }
