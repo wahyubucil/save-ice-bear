60ad6af2074db604755ee9fb49e0b673eefb0469
Wahyu Budi Saputra <wahyubucil@gmail.com>
2019-05-06 21:10:06 +0800

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'save-ice-bear';
-}
+export class AppComponent {}
