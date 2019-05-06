b9fdc09babcc419fe8e17302f888512d9ed56c46
Wahyu Budi Saputra <wahyubucil@gmail.com>
2019-05-06 22:24:22 +0800

    Base design init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { SwiperModule } from "angular2-useful-swiper";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { SliderComponent } from "./components/slider/slider.component";
+import { HeaderComponent } from "./partials/header/header.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, SliderComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, SwiperModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
