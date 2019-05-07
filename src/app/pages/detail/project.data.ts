98d620ea5e72d98a2ff13a5054d42d8fe1c8b0bb
Wahyu Budi Saputra <wahyubucil@gmail.com>
2019-05-07 08:42:50 +0800

    Change data

--- src/app/pages/detail/project.data.ts
+++ src/app/pages/detail/project.data.ts
@@ -17,22 +17,29 @@ export const ProjectData: Project[] = [
     slug: "coca-cola-is-here-to-help",
     title: "Coca Cola is here to help!",
     subtitle: "Together we can help protect their home",
-    imagePath: "../../../assets/images/we-bare-bears.jpg",
+    imagePath: "../../../assets/images/coca-cola-arctic.jpg",
     body: `
-    <p>The Bears are an adoptive family of three bears of separate species and are the face of the Cartoon Network original series We Bare Bears. The family includes the three main characters: Grizzly, Panda, and Ice Bear, each having their own personalities, interests and talents. They've been together ever since they were young cubs, as first revealed, unofficially, in the web comic The Three Bare Bears and the Pilot, and officially in the episode "Burrito".</p>
-    <p>When traveling as a stack, they usually form with Grizzly on top, Panda in the middle, and Ice Bear on the bottom, in accordance with their respective ages.</p>
-    <p>Though it is unknown how they met, it is known that they have been together since very early childhood. The only known information about the bears before they met is that Grizzly was born in a forest in the U.S and was found perched on a tree by park rangers while in distress. He was afterward adopted by a TV Network to star in Family Troubles, a Canadian Sitcom; Panda was originally from China and raised in captivity until he escaped the facility; and Ice Bear came from the Russian Arctic and was raised by Yuri until he was forced to leave him on a floating iceberg after being chased off by hunters.</p>
+      <p>Coca-Cola's drive to create a safe haven for polar bears in the high Arctic has thawed the pockets of environmentalists throughout North America, generating over $2 million for the World Wildlife Fund (WWF). Dave Moran, a spokesman for Coca-Cola Canada, called the company's Arctic Home campaign the most successful it's ever done. "And keep in mind, this is only the first year," he said.</p>
+      <p>The company partnered with WWF, the world's leading conservation organization, to help meet its goal of raising $10 million to ensure the polar bear's future. Fans of the polar bear and Coca-Cola responded by texting the package code, triggering $1 donations that quickly amounted to $1.8 million. The company contributed an additional $400,000 as part of its five-year, $2 million commitment to the cause.</p>
+      <p><strong>Where the Money Goes</strong></p>
+      <p>The loss of sea ice due to the changing environment means polar bears are vulnerable in their current habitat. "As the Arctic goes, so goes the polar bear," said Carter Roberts, president and CEO of WWF. "Precious polar bear habitat is disappearing at an alarming rate, and without intervention, so will this magnificent bear. The success of Arctic Home gives me hope for the polar bear's future."</p>
+      <p>WWF has a vision to work with the Inuit and other local residents to manage an area high in the Arctic where the summer sea ice will likely persist the longest. Polar bears need sea ice platforms for hunting opportunities, to breed and to rest. As climate change melts sea ice and moves the platforms farther apart, the U.S. Geological Survey projects that two-thirds of polar bears will disappear by 2050.</p>
+      <p><strong>Coke's Longtime Commitment to the Polar Bear and Sustainability</strong></p>
+      <p>The polar bear has been a beloved Coca-Cola icon since appearing in a print ad in 1922. The Coca-Cola Polar Bear first appeared in a commercial called "Northern Lights" in 1993 and has since turned up in commercials ice skating or gazing at the moon. Earlier this year, Coke introduced new polar bear ads during the Super Bowl, further underscoring the company's affection and commitment to the animal.</p>
+      <p>The Arctic Home project grew out of an existing partnership between WWF and Coca-Cola focused on freshwater conservation and a mutual interest in protecting the polar bear's habitat. Coca-Cola has supported WWF's polar bear conservation efforts for the past four years.</p>
     `
   },
   {
     slug: "wwf-always-the-first",
     title: "WWF Always the First!",
     subtitle: "Prevent Global Warming. To save their Home!",
-    imagePath: "../../../assets/images/we-bare-bears.jpg",
+    imagePath: "../../../assets/images/wwf-polar-bear.jpg",
     body: `
-    <p>The Bears are an adoptive family of three bears of separate species and are the face of the Cartoon Network original series We Bare Bears. The family includes the three main characters: Grizzly, Panda, and Ice Bear, each having their own personalities, interests and talents. They've been together ever since they were young cubs, as first revealed, unofficially, in the web comic The Three Bare Bears and the Pilot, and officially in the episode "Burrito".</p>
-    <p>When traveling as a stack, they usually form with Grizzly on top, Panda in the middle, and Ice Bear on the bottom, in accordance with their respective ages.</p>
-    <p>Though it is unknown how they met, it is known that they have been together since very early childhood. The only known information about the bears before they met is that Grizzly was born in a forest in the U.S and was found perched on a tree by park rangers while in distress. He was afterward adopted by a TV Network to star in Family Troubles, a Canadian Sitcom; Panda was originally from China and raised in captivity until he escaped the facility; and Ice Bear came from the Russian Arctic and was raised by Yuri until he was forced to leave him on a floating iceberg after being chased off by hunters.</p>
+      <p><strong>WWF’s goal is to ensure that whatever development takes place in the Arctic is sustainable, and that it does not damage wildlife populations and ecosystems to any great extent.</strong></p>
+      <p>WWF accepts that it is unreasonable to designate the whole Arctic as a place of no industrial activity. The Arctic is not like the Antarctic; it is occupied by more than four million people. While some of these people continue subsistence lifestyles, others want a place in the modern wage economy which often means jobs from exploitation of non-renewable resources.</p>
+      <p>We analyze the technical capacities available to prevent and respond to oil spills, and advocate for the highest development standards through national and international venues.</p>
+      <p><strong>To most of the world, the polar bear is a big, white, fluffy, huggable animal. To people who live with polar bears, these animals are large and dangerous predators.</strong></p>
+      <p>Keeping polar bears separate from people is better for both—as polar bears that wander into communities pose a risk to people, and people often respond by killing the bears. WWF has responded with a variety of locally-led initiatives to help reduce conflict.</p>
     `
   }
 ];
